0cb56443bada3bc403eabb339100df4c38d1cffb
Andre Schepers <andreschepers81@gmail.com>
2015-08-03 20:42:27 +0200

    Added xml-xsd projects to create and read xml defined lessons

---
+++ LessonCreator/src/main/java/eu/filfa/lessoncreator/VocabularyXMLApi.java
@@ -0,0 +1,247 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.filfa.lessoncreator;
+
+import java.io.File;
+import java.util.Arrays;
+import javax.xml.validation.SchemaFactory;
+import org.xml.sax.SAXException;
+import generated.Lesson;
+import generated.ObjectFactory;
+import generated.Vocabularytype;
+import generated.Iterationstype;
+import generated.Instructionstype;
+import generated.Iterationtype;
+import generated.Presentationtype;
+import generated.Vocelemtype;
+import generated.Columnordertype;
+import generated.Pairstype;
+import generated.Pairtype;
+import generated.Pairelemtype;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Andre Schepers andreschepers81@gmail.com
+ */
+public class VocabularyXMLApi {
+    
+    /**
+     * 
+     */
+    private static Lesson lesson = new Lesson();
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        lesson.setId("1");
+        lesson.setName("Alphabet");
+        
+        
+        ObjectFactory obFac = new ObjectFactory();
+        lesson.getContent().add(obFac.createLessonVocabulary(getVocabulary()));
+        
+        // Code template start.
+        try {
+            javax.xml.bind.JAXBContext jaxbCtx = javax.xml.bind.JAXBContext.newInstance(lesson.getClass().getPackage().getName());
+            javax.xml.bind.Marshaller marshaller = jaxbCtx.createMarshaller();
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"); //NOI18N
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_NO_NAMESPACE_SCHEMA_LOCATION, "lesson.xsd");
+            SchemaFactory sf = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+            marshaller.setSchema(sf.newSchema(new File("src/main/resources/lesson.xsd")));
+            marshaller.marshal(lesson, System.out);
+        } catch (javax.xml.bind.JAXBException ex) {
+            System.err.println("Went something wrong with marshalling the custom xml piece of shit of code: " + ex.getMessage());
+            System.err.println(ex.toString());
+            System.err.println(Arrays.toString(ex.getStackTrace()));
+        } catch (SAXException ex) {
+            Logger.getLogger(VocabularyXMLApi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Vocabularytype getVocabulary() {
+        Vocabularytype voc = new Vocabularytype();
+        voc.setInstructions(getInstructions());
+        voc.setPairs(getPairs());
+        return voc;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Instructionstype getInstructions() {
+        Instructionstype instructions = new Instructionstype();
+        instructions.setIterations(getIterations());
+        return instructions;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Iterationstype getIterations() {
+        // List of iterations
+        Iterationstype iterations = new Iterationstype();
+        iterations.getIteration().add(getIteration1());
+        iterations.getIteration().add(getIteration2());
+        iterations.getIteration().add(getIteration3());
+        iterations.getIteration().add(getIteration4());
+        return iterations;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Iterationtype getIteration1() {
+        // Iteration 1
+        Iterationtype iteration1 = new Iterationtype();
+        iteration1.setIndex(0);
+        Presentationtype presType1 = new Presentationtype();
+        iteration1.setQuery(presType1);
+        presType1.setType(Vocelemtype.IMAGE);
+        Presentationtype presType2 = new Presentationtype();
+        iteration1.setOptions(presType2);
+        presType2.setType(Vocelemtype.TEXT);
+        iteration1.setColumnorder(Columnordertype.FIRST_FIRST);
+        return iteration1;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Iterationtype getIteration2() {
+        // Iteration 2
+        Iterationtype iteration2 = new Iterationtype();
+        iteration2.setIndex(1);
+        Presentationtype presType1 = new Presentationtype();
+        iteration2.setQuery(presType1);
+        presType1.setType(Vocelemtype.TEXT);
+        Presentationtype presType2 = new Presentationtype();
+        iteration2.setOptions(presType2);
+        presType2.setType(Vocelemtype.TEXT);
+        iteration2.setColumnorder(Columnordertype.SECOND_FIRST);
+        return iteration2;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Iterationtype getIteration3() {
+        // Iteration 3
+        Iterationtype iteration3 = new Iterationtype();
+        iteration3.setIndex(2);
+        Presentationtype presType1 = new Presentationtype();
+        iteration3.setQuery(presType1);
+        presType1.setType(Vocelemtype.TEXT);
+        Presentationtype presType2 = new Presentationtype();
+        iteration3.setOptions(presType2);
+        presType2.setType(Vocelemtype.TEXT);
+        iteration3.setColumnorder(Columnordertype.FIRST_SECOND);
+        return iteration3;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Iterationtype getIteration4() {
+        // Iteration 4
+        Iterationtype iteration4 = new Iterationtype();
+        iteration4.setIndex(3);
+        Presentationtype presType1 = new Presentationtype();
+        iteration4.setQuery(presType1);
+        presType1.setType(Vocelemtype.TEXT);
+        Presentationtype presType2 = new Presentationtype();
+        iteration4.setOptions(presType2);
+        presType2.setType(Vocelemtype.TEXT);
+        iteration4.setColumnorder(Columnordertype.SECOND_SECOND);
+        return iteration4;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Pairstype getPairs() {
+        Pairstype pairs = new Pairstype();
+        pairs.getPair().add(getPair1());
+        pairs.getPair().add(getPair2());
+        pairs.getPair().add(getPair3());
+        pairs.getPair().add(getPair4());
+        return pairs;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Pairtype getPair1() {
+        Pairtype pair = new Pairtype();
+        Pairelemtype first = new Pairelemtype();
+        first.setText("test");
+        pair.setFirst(first);
+        Pairelemtype second = new Pairelemtype();
+        second.setText("test");
+        pair.setSecond(second);
+        return pair;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Pairtype getPair2() {
+        Pairtype pair = new Pairtype();
+        Pairelemtype first = new Pairelemtype();
+        first.setText("test");
+        pair.setFirst(first);
+        Pairelemtype second = new Pairelemtype();
+        second.setText("test");
+        pair.setSecond(second);
+        return pair;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Pairtype getPair3() {
+        Pairtype pair = new Pairtype();
+        Pairelemtype first = new Pairelemtype();
+        first.setText("test");
+        pair.setFirst(first);
+        Pairelemtype second = new Pairelemtype();
+        second.setText("test");
+        pair.setSecond(second);
+        return pair;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    private static Pairtype getPair4() {
+        Pairtype pair = new Pairtype();
+        Pairelemtype first = new Pairelemtype();
+        first.setText("test");
+        pair.setFirst(first);
+        Pairelemtype second = new Pairelemtype();
+        second.setText("test");
+        pair.setSecond(second);
+        return pair;
+    }
+}
---
+++ src/main/java/com/mycompany/vocabularytrainer/domain/DefaultDecorableVocabularyElementPair.java
@@ -0,0 +1,204 @@
+/**
+ * VocabularyTrainer  Copyright (C) 2015  André Schepers
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.mycompany.vocabularytrainer.domain;
+
+import com.mycompany.vocabularytrainer.domain.interfaces.DecorableRepresentative;
+import com.mycompany.vocabularytrainer.domain.interfaces.DecorableVocabularyElementPair;
+import com.mycompany.vocabularytrainer.domain.interfaces.VocabularyElementPair;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ *
+ * @author andres81
+ */
+public class DefaultDecorableVocabularyElementPair implements DecorableVocabularyElementPair {
+    
+    // Logging
+    private static final Logger logger = LogManager.getLogger(DefaultDecorableVocabularyElementPair.class);
+    
+    /**
+     * 
+     */
+    private final DecorableRepresentative entryOne;
+    
+    /**
+     * 
+     */
+    private final DecorableRepresentative entryTwo;
+    
+    /**
+     * 
+     */
+    private final UUID uuid;
+    
+    /**
+     * 
+     */
+    private Map<Object,Object> decorators = null;
+    
+    /**
+     * 
+     * @param uuid
+     * @param first
+     * @param second
+     */
+    public DefaultDecorableVocabularyElementPair(UUID uuid, DecorableRepresentative first, DecorableRepresentative second) {
+        if (uuid == null ||
+            first == null ||
+            second == null) {
+            throw new NullPointerException();
+        }
+        this.uuid = uuid;
+        entryOne = first;
+        entryTwo = second;
+        decorators = new HashMap<>();
+    }
+    
+    /**
+     * 
+     * @param pair 
+     */
+    public DefaultDecorableVocabularyElementPair(VocabularyElementPair pair) {
+        if (pair.getUuid() == null ||
+            pair.getFirst() == null ||
+            pair.getSecond() == null) {
+            throw new NullPointerException();
+        }
+        this.uuid = pair.getUuid();
+        entryOne = pair.getFirst();
+        entryTwo = pair.getSecond();
+        decorators = new HashMap<>();
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public DecorableRepresentative getFirst() {
+        return entryOne;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public DecorableRepresentative getSecond() {
+        return entryTwo;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public UUID getUuid() {
+        return uuid;
+    }
+    
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof DefaultDecorableVocabularyElementPair) {
+            DefaultDecorableVocabularyElementPair pair = (DefaultDecorableVocabularyElementPair) obj;
+            return uuid == pair.getUuid();
+        }
+        return false;
+    }
+
+    /**
+     * 
+     * @return 
+     */
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 67 * hash + Objects.hashCode(this.uuid);
+        return hash;
+    }
+
+    @Override
+    public int size() {
+        return decorators.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return decorators.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return decorators.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return decorators.containsValue(value);
+    }
+
+    @Override
+    public Object get(Object key) {
+        return decorators.get(key);
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return decorators.put(key, value);
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return decorators.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends Object, ? extends Object> m) {
+        decorators.putAll(m);
+    }
+
+    @Override
+    public void clear() {
+        decorators.clear();
+    }
+
+    @Override
+    public Set<Object> keySet() {
+        return decorators.keySet();
+    }
+
+    @Override
+    public Collection<Object> values() {
+        return decorators.values();
+    }
+
+    @Override
+    public Set<Entry<Object, Object>> entrySet() {
+        return decorators.entrySet();
+    }
+}
